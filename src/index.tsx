542a9f5fc41d73164703f4e0c7626de3ba2c3120
mecaota <24618982+mecaota@users.noreply.github.com>
2024-04-19 17:58:42 +0900

    wrap Suspense component by div tag

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,13 @@ import { Suspense, lazy } from 'preact/compat';
 
 const Empty = lazy(() => import('@components/empty'));
 
-export const App: FunctionalComponent = () => <Suspense fallback={<p>now loading</p>}><Empty loading /></Suspense>;
+export const App: FunctionalComponent = () => (
+  <div>
+    <Suspense fallback={<p>now loading</p>}>
+      <Empty loading />
+    </Suspense>
+  </div>
+);
 
 const target = document.getElementById('root');
 target && render(<App />, target);
